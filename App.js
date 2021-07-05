f0fabc09ebb59f2423afab319185aa47f5160b11
Mahesh Katre <mahesh.katre47@gmail.com>
2021-07-05 18:43:29 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import MyPaint from './sketchDraw';
 
 export default function App() {
+  const images = [
+    { id: "1", src: require('./images/1.jpeg'), title: 'foo', description: 'bar' },
+    { id: "2", src: require('./images/2.jpeg'), title: 'foo', description: 'bar' },
+    { id: "3", src: require('./images/3.jpeg'), title: 'foo', description: 'bar' },
+    { id: "4", src: require('./images/4.jpeg'), title: 'foo', description: 'bar' },
+    { id: "5", src: require('./images/5.jpeg'), title: 'foo', description: 'bar' },
+  ];
+  const renderItem = ({item}) => {
+    return <Image
+              key={item.id}
+              style={{margin: 40,width:'80%',}}
+              source={item.src} />
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+        style={{width: '100%', height:'100%'}}
+        data={images}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+      {/* <MyPaint /> */}
       <StatusBar style="auto" />
     </View>
   );
